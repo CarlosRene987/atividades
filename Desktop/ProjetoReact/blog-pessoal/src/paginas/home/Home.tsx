289df0115c7f5a517c6c5394c4aa062e1921f556
unknown <carlosrene.rene01@gmail.com>
2022-07-08 15:17:24 -0300

    Atualização Blog

--- Desktop/ProjetoReact/blog-pessoal/src/paginas/home/Home.tsx
+++ Desktop/ProjetoReact/blog-pessoal/src/paginas/home/Home.tsx
@@ -1,28 +1,31 @@
 
 import React from 'react';
+import { Grid, Button, Typography} from '@material-ui/core';  
+import { Box } from '@mui/material';
+
 import './Home.css';
 
-function Home(){
-    
+function Home() {
     return (
         <>
-            <h1 className="titulo">Bem Vindo</h1>
-            <h2 className="titulo">Olá sinta-se a vontade para conhecer o nosso site de estilos musicais</h2>
-            <img src="https://i.imgur.com/H88yIo2.png" alt="Imagem Tela Inicial" className="img"/>
-            <h2 className="temas">3 Estilos musicais</h2>
-            <img src="https://static.todamateria.com.br/upload/sa/mb/sambacaribe-cke.jpg?auto_optimize=low" alt="Imagem Tela Inicial" className="img"/>
-                    <h2 className="temas">MPB</h2>
-                    <h3 className="temas"> Música popular brasileira resulta de um conjunto de manifestações culturais sob a influência da cultura indígena, africana, e europeia.</h3>
-                    <h2 className="temas">Música da Clarissa Falcão</h2>
-                    <iframe width="560" height="315" src="https://www.youtube.com/embed/rDAd302a2lc" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-                    <h2 className="temas">ROCK NACIONAL</h2>
-                    <h3 className="temas"> Refere-se ao tratamento do estilo musical Rock dos Estados Unidos para a linguagem do Brasil deu-se em início principalmente nos anos de 1950</h3>
-                    <h2 className="temas">Música da Pitty</h2>
-                   <iframe width="560" height="315" src="https://www.youtube.com/embed/His5BZcgxj8" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-                   <h2 className="temas">JAZZ</h2>
-                    <h3 className="temas"> No Brasil é caracterizada principalmente pelas musicas instrumentais surgiu principalmente nos anos 60 com a revolução da Bossa Nova</h3>
-                    <h2 className="temas">Música da Bossa Nova</h2>
-                    <iframe width="560" height="315" src="https://www.youtube.com/embed/fT3W2LTZ9Ks" title="Ary Barroso,Chico Buarque e Celia..." frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+            <Grid container direction="row" justifyContent="center" alignItems="center" style={{ backgroundColor: "#3F51B5" }}>
+                <Grid alignItems="center" item xs={6}>
+                    <Box paddingX={20} >
+                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" style={{ color: "white", fontWeight: "bold" }}>Seja bem vindo(a)!</Typography>
+                        <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" style={{ color: "white", fontWeight: "bold" }}>expresse aqui os seus pensamentos e opiniões!</Typography>
+                    </Box>
+                    <Box display="flex" justifyContent="center">
+                        <Box marginRight={1}>
+                        </Box>
+                        <Button variant="outlined" style={{ borderColor: "white", backgroundColor: "#3F51B5", color: "white" }}>Ver Postagens</Button>
+                    </Box>
+                </Grid>
+                <Grid item xs={6} >
+                    <img src="https://i.imgur.com/H88yIo2.png" alt="" width="500px" height="500px" />
+                </Grid>
+                <Grid xs={12} style={{ backgroundColor: "white" }}>
+                </Grid>
+            </Grid>
         </>
     );
 }
